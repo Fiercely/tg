1f2df5742478f083f3f61560096f57ac29ef8e9b
André Ilhicas dos Santos <andre.ilhicas@fiercely.pt>
2017-04-12 14:43:51 +0100

    FIX typo in build-package.sh

--- build-package.sh
+++ build-package.sh
@@ -3,7 +3,8 @@
 #and compiling it, resulting in:
 #telegram_cli_xxx_$ARCH.deb
 #Installing build dependencies 
-apt get -y install build-essential debhelper autotools-dev autoconf-archive libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev libjansson-dev libpython-dev make 
+apt-get update
+apt-get -y install build-essential debhelper autotools-dev autoconf-archive libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev libjansson-dev libpython-dev make 
 #Building package
 dpkg-buildpackage -b -rfakeroot -us -uc 
 
