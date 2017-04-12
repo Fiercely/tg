3ec4ed1ce313f7efa88dddc341d8a6d71af66e11
André Ilhicas dos Santos <andre.ilhicas@fiercely.pt>
2017-04-12 14:38:22 +0100

    FIX build-package.sh to include dependencies to install

--- build-package.sh
+++ build-package.sh
@@ -2,5 +2,9 @@
 #This Script will build the debian package with only binary files according to the architecture of the machinerunning this Script
 #and compiling it, resulting in:
 #telegram_cli_xxx_$ARCH.deb
-dpkg-buildpackage -b -rfakeroot -us -uc
+#Installing build dependencies 
+apt get -y install build-essential debhelper autotools-dev autoconf-archive libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev libevent-dev libjansson-dev libpython-dev make 
+#Building package
+dpkg-buildpackage -b -rfakeroot -us -uc 
+
 
